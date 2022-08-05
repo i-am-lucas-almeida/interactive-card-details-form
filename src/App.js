967621d83804cb17560facd300448a56891fa6f2
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-08-05 20:52:34 -0300

    Update App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
+import { useState } from "react";
+
+import CardSection from "./components/CardSection";
+import Container from "./components/Container";
+import Form from "./components/Form/Form";
+
 const App = () => {
+
+  const initState = {
+    cardName: "",
+    cardNumber: "",
+    validMonth: "",
+    validYear: "",
+    securityNumber: "",
+    touched: {
+      cardName: false,
+      cardNumber: false,
+      validMonth: false,
+      validYear: false,
+      securityNumber: false
+    }
+  };
+
+  const [formData, setFormData] = useState(initState);
+
+  function handleChange(e) {
+
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+
+  }
+
+  function handleBlur(e) {
+
+    const { name } = e.target;
+    setFormData({ ...formData, touched: { ...formData.touched, [name]: true } });
+
+  }
 
   return (
 
-    <p>App</p>
+    <Container>
+
+      <CardSection
+        formData={formData}
+      />
+
+      <Form
+        formData={formData}
+        handleBlur={handleBlur}
+        handleChange={handleChange}
+      />
+
+    </Container>
 
   );
 
