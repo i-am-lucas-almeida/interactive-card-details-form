47559ec68248902a65c7a3c08ef73a2a56b2ed12
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-08-06 01:36:37 -0300

    Update validateForm.js

--- src/components/Form/validateForm.js
+++ src/components/Form/validateForm.js
@@ -4,6 +4,8 @@ export const validate = (formData) => {
         cardName: "",
         cardNumber: "",
         date: "",
+        validMonth: "",
+        validYear: "",
         securityNumber: ""
     };
 
@@ -19,27 +21,37 @@ export const validate = (formData) => {
     if (formData.touched.cardNumber && !formData.cardNumber) {
         errors.cardNumber = "Campo obrigatório";
 
-    } else if (formData.touched.cardNumber && formData.cardNumber.length < 16) {
+    } else if (formData.touched.cardNumber && (formData.cardNumber.replace(/ /g, "")).length < 16) {
         errors.cardNumber = "Número inválido";
     }
 
     if (formData.touched.validMonth && !formData.validMonth) {
         errors.date = "Campo obrigatório";
+        errors.validMonth = "Campo obrigatório";
 
     } else if (formData.touched.validYear && !formData.validYear) {
         errors.date = "Campo obrigatório";
+        errors.validYear = "Campo obrigatório";
 
     } else if (formData.touched.validMonth && formData.validMonth === "0") {
         errors.date = "Data inválida";
+        errors.validMonth = "Data inválida";
 
     } else if (formData.touched.validMonth && formData.validMonth === "00") {
         errors.date = "Data inválida";
+        errors.validMonth = "Data inválida";
 
-    } else if (formData.touched.validMonth && formData.validMonth > 12) {
+    } else if (formData.touched.validMonth && parseInt(formData.validMonth) > 12) {
         errors.date = "Data inválida";
+        errors.validMonth = "Data inválida";
 
     } else if (formData.touched.validYear && formData.validYear === "0") {
         errors.date = "Data inválida";
+        errors.validYear = "Data inválida";
+
+    } else if (formData.touched.validYear && parseInt(formData.validYear) < 22) {
+        errors.date = "Data inválida";
+        errors.validYear = "Data inválida";
 
     }
 
