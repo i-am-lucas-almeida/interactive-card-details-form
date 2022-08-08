5bd657b3e0c63e3d73a14b58bd91789c244489e0
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-08-08 17:11:25 -0300

    Update validateForm.js

--- src/components/Form/validateForm.js
+++ src/components/Form/validateForm.js
@@ -11,12 +11,21 @@ export const validate = (formData) => {
 
     const validName = /^[a-zA-ZÀ-Ÿ][A-zÀ-ÿ']+\s([A-zÀ-ÿ']\s?)*[a-zA-ZÀ-Ÿ][A-zÀ-ÿ']+$/;
 
+    // get current year
+
+    const getYear = new Date().getFullYear().toString();
+    const currentYear = getYear.substring(2, 4);
+
+    // validate card name
+
     if (formData.touched.cardName && !formData.cardName) {
         errors.cardName = "Campo obrigatório";
 
     } else if (formData.touched.cardName && !validName.test(formData.cardName)) {
         errors.cardName = "Nome inválido";
     }
+
+    // validate card number
 
     if (formData.touched.cardNumber && !formData.cardNumber) {
         errors.cardNumber = "Campo obrigatório";
@@ -25,13 +34,11 @@ export const validate = (formData) => {
         errors.cardNumber = "Número inválido";
     }
 
+    // validate card date
+
     if (formData.touched.validMonth && !formData.validMonth) {
         errors.date = "Campo obrigatório";
         errors.validMonth = "Campo obrigatório";
-
-    } else if (formData.touched.validYear && !formData.validYear) {
-        errors.date = "Campo obrigatório";
-        errors.validYear = "Campo obrigatório";
 
     } else if (formData.touched.validMonth && formData.validMonth === "0") {
         errors.date = "Data inválida";
@@ -45,15 +52,23 @@ export const validate = (formData) => {
         errors.date = "Data inválida";
         errors.validMonth = "Data inválida";
 
+    }
+
+    if (formData.touched.validYear && !formData.validYear) {
+        errors.date = "Campo obrigatório";
+        errors.validYear = "Campo obrigatório";
+
     } else if (formData.touched.validYear && formData.validYear === "0") {
         errors.date = "Data inválida";
         errors.validYear = "Data inválida";
 
-    } else if (formData.touched.validYear && parseInt(formData.validYear) < 22) {
+    } else if (formData.touched.validYear && parseInt(formData.validYear) < parseInt(currentYear)) {
         errors.date = "Data inválida";
         errors.validYear = "Data inválida";
 
     }
+
+    // validate security code
 
     if (formData.touched.securityNumber && !formData.securityNumber) {
         errors.securityNumber = "Campo obrigatório";
