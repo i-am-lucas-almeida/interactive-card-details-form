52b1894ca4549c76a568a049d7332487a54ceb5f
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-08-08 17:11:20 -0300

    Update Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,18 @@
 import styles from "../../sass/components/Form.module.scss";
+import "animate.css";
+
+import { useState } from "react";
 
 import { validate } from "./validateForm";
 import { NumberMask, CardNumberMask } from "../../utils/InputMask";
 
+import { FormLoading } from "../Form/Loading";
+import SuccessMessage from "../SuccessMessage";
+
 const Form = ({ formData, handleBlur, handleChange }) => {
+
+    const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState(false);
 
     const { cardName, cardNumber, validMonth, validYear, securityNumber } = formData;
 
@@ -15,125 +24,165 @@ const Form = ({ formData, handleBlur, handleChange }) => {
 
         e.preventDefault();
 
-        console.log(formData);
+        if ((!errors.cardName) &&
+            (!errors.cardNumber) &&
+            (!errors.date) &&
+            (!errors.securityNumber)) {
+
+            setLoading(true);
+
+            setInterval(() => {
+
+                setLoading(false);
+                setSuccess(true);
+
+            }, 3000);
+
+        }
 
     }
 
     return (
 
-        <form onSubmit={onSubmit} className={styles.form}>
+        <div className={styles.container}>
 
-            <fieldset>
+            {!success ?
 
-                <label>Nome do cartão</label>
+                <form onSubmit={onSubmit} className={styles.form}>
 
-                <input
-                    type="text"
-                    name="cardName"
-                    maxLength={20}
-                    size={20}
-                    placeholder="ex. Isabele Acciolo"
-                    value={cardName}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    required
-                    className={errors.cardName ? styles.error : ""}
-                />
+                    <fieldset>
 
-                {errors.cardName && <small>{errors.cardName}</small>}
-
-            </fieldset>
-
-            <fieldset>
-
-                <label>Número do cartão</label>
-
-                <input
-                    type="text"
-                    name="cardNumber"
-                    maxLength={22}
-                    size={22}
-                    placeholder="ex. 1234 5678 9999 0000"
-                    value={cardNumber}
-                    onChange={(e) => handleChange(CardNumberMask(e))}
-                    onBlur={handleBlur}
-                    required
-                    className={errors.cardNumber ? styles.error : ""}
-                />
-
-                {errors.cardNumber && <small>{errors.cardNumber}</small>}
-
-            </fieldset>
-
-            <div className={styles.form__bottom}>
-
-                <fieldset>
-
-                    <label>Data valid. (MM/AA)</label>
-
-                    <div className={styles.form__date}>
+                        <label>Nome do cartão</label>
 
                         <input
                             type="text"
-                            name="validMonth"
-                            maxLength={2}
-                            size={2}
-                            placeholder="MM"
-                            value={validMonth}
-                            onChange={(e) => handleChange(NumberMask(e))}
+                            name="cardName"
+                            maxLength={20}
+                            minLength={5}
+                            size={20}
+                            placeholder="ex. Isabele Acciolo"
+                            value={cardName}
+                            onChange={handleChange}
                             onBlur={handleBlur}
                             required
-                            className={errors.validMonth ? styles.error : ""}
+                            className={errors.cardName ? styles.error : ""}
                         />
+
+                        {errors.cardName && <small>{errors.cardName}</small>}
+
+                    </fieldset>
+
+                    <fieldset>
+
+                        <label>Número do cartão</label>
 
                         <input
                             type="text"
-                            name="validYear"
-                            maxLength={2}
-                            size={2}
-                            placeholder="AA"
-                            value={validYear}
-                            onChange={(e) => handleChange(NumberMask(e))}
+                            name="cardNumber"
+                            maxLength={22}
+                            minLength={22}
+                            size={22}
+                            placeholder="ex. 1234 5678 9999 0000"
+                            value={cardNumber}
+                            onChange={(e) => handleChange(CardNumberMask(e))}
                             onBlur={handleBlur}
                             required
-                            className={errors.validYear ? styles.error : ""}
+                            className={errors.cardNumber ? styles.error : ""}
                         />
+
+                        {errors.cardNumber && <small>{errors.cardNumber}</small>}
+
+                    </fieldset>
+
+                    <div className={styles.form__bottom}>
+
+                        <fieldset>
+
+                            <label>Data valid. (MM/AA)</label>
+
+                            <div className={styles.form__date}>
+
+                                <input
+                                    type="text"
+                                    name="validMonth"
+                                    maxLength={2}
+                                    minLength={2}
+                                    size={2}
+                                    placeholder="MM"
+                                    value={validMonth}
+                                    onChange={(e) => handleChange(NumberMask(e))}
+                                    onBlur={handleBlur}
+                                    required
+                                    className={errors.validMonth ? styles.error : ""}
+                                />
+
+                                <input
+                                    type="text"
+                                    name="validYear"
+                                    maxLength={2}
+                                    minLength={2}
+                                    size={2}
+                                    placeholder="AA"
+                                    value={validYear}
+                                    onChange={(e) => handleChange(NumberMask(e))}
+                                    onBlur={handleBlur}
+                                    required
+                                    className={errors.validYear ? styles.error : ""}
+                                />
+
+                            </div>
+
+                            {errors.date && <small>{errors.date}</small>}
+
+                        </fieldset>
+
+                        <fieldset className={styles.form__security_code}>
+
+                            <label>CVV</label>
+
+                            <input
+                                type="text"
+                                name="securityNumber"
+                                maxLength={3}
+                                minLength={3}
+                                size={3}
+                                placeholder="ex. 123"
+                                value={securityNumber}
+                                onChange={(e) => handleChange(NumberMask(e))}
+                                onBlur={handleBlur}
+                                required
+                                className={errors.securityNumber ? styles.error : ""}
+                            />
+
+                            {errors.securityNumber && <small>{errors.securityNumber}</small>}
+
+                        </fieldset>
 
                     </div>
 
-                    {errors.date && <small>{errors.date}</small>}
+                    {!loading &&
+                        <button type="submit"
+                            className={styles.form__button}>
+                            Confirmar
+                        </button>
+                    }
 
-                </fieldset>
+                    {loading &&
+                        <button type="submit"
+                            className={`${styles.form__button} ${styles.form__button_loading}`} disabled>
+                            <FormLoading />
+                        </button>
+                    }
 
-                <fieldset className={styles.form__security_code}>
+                </form>
 
-                    <label>CVV</label>
+                :
 
-                    <input
-                        type="text"
-                        name="securityNumber"
-                        maxLength={3}
-                        size={3}
-                        placeholder="ex. 123"
-                        value={securityNumber}
-                        onChange={(e) => handleChange(NumberMask(e))}
-                        onBlur={handleBlur}
-                        required
-                        className={errors.securityNumber ? styles.error : ""}
-                    />
+                <SuccessMessage />
 
-                    {errors.securityNumber && <small>{errors.securityNumber}</small>}
+            }
 
-                </fieldset>
-
-            </div>
-
-            <button type="submit"
-                className={styles.form__button}>
-                Confirmar
-            </button>
-
-        </form>
+        </div>
 
     );
 
